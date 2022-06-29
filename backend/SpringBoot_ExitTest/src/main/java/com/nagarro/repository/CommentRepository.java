f35f3a7fb33534863b3ff966dc2c6657302f5ebd
atishayjain <atishay.jain@nagarro.com>
2022-06-29 18:28:53 +0530

    assignment

---
+++ backend/SpringBoot_ExitTest/src/main/java/com/nagarro/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.nagarro.repository;
+
+import com.nagarro.model.CommentModel;
+import com.nagarro.model.QuestionModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<CommentModel, Integer> {
+
+    @Query(value = "select * from comment where " +
+            " parentid = :pid " , nativeQuery = true)
+    List<CommentModel> searchComments(Integer pid);
+//qsnid
+}
